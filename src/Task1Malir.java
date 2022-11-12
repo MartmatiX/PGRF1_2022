69288060bec9cd80f38dd4be38656a84dc6b0e17
MartmatiX <martin.malir@gmail.com>
2022-11-12 15:20:05 +0100

    github link update

--- src/Task1Malir.java
+++ src/Task1Malir.java
@@ -1,6 +1,6 @@
 public class Task1Malir {
 
-    // github:  https://github.com/MartmatiX/PGRF1_Tasks
+    // github:  https://github.com/MartmatiX/PGRF1_2022
 
     public static void main(String[] args) {
         Canvas canvas = new Canvas(1280, 720);
