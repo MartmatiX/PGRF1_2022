9e340ad49b567d7e752c2f09baae73c1288ba00d
MartmatiX <martin.malir@gmail.com>
2022-11-14 22:24:38 +0100

    Polygon cropper beginning

---
+++ src/model/CroppedPolygon.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.List;
+
+public class CroppedPolygon {
+
+    public Polygon croppedPolygon(Polygon polygon1, Polygon polygon2){
+        List<Edge> polygon1Edges = polygon1.getEdges();
+        List<Edge> polygon2Edges = polygon2.getEdges();
+
+        return new Polygon();
+    }
+
+}
--- src/model/Polygon.java
+++ src/model/Polygon.java
@@ -15,6 +15,23 @@ public class Polygon {
         return points;
     }
 
+    public List<Edge> getEdges(){
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            int nextIndex = (i + 1) % points.size();
+            Point p1 = points.get(i);
+            Point p2 = points.get(nextIndex);
+            Edge edge = new Edge(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+            if (edge.isHorizontal())
+                continue;
+
+            // pridam hranu do seznamu
+            edge.orientate();
+            edges.add(edge);
+        }
+        return edges;
+    }
+
     public int getCount() {
         return points.size();
     }
